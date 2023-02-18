c984e5e95121d65f49f24dee2892cc881a631575
Pedro Pauluci <pauluci125romao.pedro@hotmail.com>
2023-02-18 20:10:23 -0300

    Trabalhando com rotas dinamicas

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,31 @@ Route::get('/contato', function () {
     echo "Conteudo dinamico Contato";
 });
 
-Route::get('/servicos', function () {
-    echo "Conteudo dinamico Servicos";
+Route::get('/servicos/{id}', function (int $id) {
+    echo "Estou mostrando o serviço com id $id";
+
+    $servicos = [
+        1 =>[
+            'nome' => 'Lavagem de roupa',
+            'descricao' => 'descricao muito longa'
+        ],
+        2 =>[
+            'nome' => 'Lavagem de roupa',
+            'descricao' => 'descricao muito lonmga'
+        ],
+        3 =>[
+            'nome' => 'Lavagem de roupa',
+            'descricao' => 'descricao muito lonmga'
+        ],
+    ];
+
+    echo $servicos[$id]['nome'];
+    echo "<br>";
+    echo $servicos[$id]['descricao'];
 });
 
+
+Route::get('/saudacoes/{nome}', function (string $nome = 'TreinaWeb') {
+    echo "Ola $nome";
+});
 
